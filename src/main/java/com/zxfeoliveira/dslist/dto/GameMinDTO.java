f94d737a629185a140a76a5db7f4c60966edca25
Felipe Oliveira <fbmithrandir@outlook.com>
2023-07-10 23:39:11 -0300

    GameRepository 2

--- src/main/java/com/zxfeoliveira/dslist/dto/GameMinDTO.java
+++ src/main/java/com/zxfeoliveira/dslist/dto/GameMinDTO.java
@@ -20,6 +20,26 @@ public class GameMinDTO {
 		this.imgUrl = entity.getImgUrl();
 		this.shortDescription = entity.getShortDescription();
 	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public Integer getYear() {
+		return year;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public String getShortDescription() {
+		return shortDescription;
+	}
 	
 	
 }
